2b7af7b24a65b99190ea3f4f2a73bfbb7bcd9236
Elkulas <elkulasjiang@gmail.com>
2020-06-24 20:51:39 +0800

    finish lec5.2

--- Lec_05/code/src/E2Rt.cpp
+++ Lec_05/code/src/E2Rt.cpp
@@ -29,16 +29,40 @@ int main(int argc, char **argv) {
 
     // SVD and fix sigular values
     // START YOUR CODE HERE
-
+    Eigen::JacobiSVD<Matrix3d> svd(E, ComputeFullU | ComputeFullV);
+    Eigen::Vector3d vsigma = svd.singularValues();
+    Eigen::Matrix3d U = svd.matrixU();
+    Eigen::Matrix3d V = svd.matrixV();
+    cout << "sigma = " << vsigma << endl;
+    // 将sigma从vector形式转化成矩阵形式
+    Eigen::Matrix3d msigma;
+    msigma << (vsigma(0,0) + vsigma(1,0))/2, 0, 0,
+               0, (vsigma(1,0) + vsigma(0,0))/2, 0,
+               0 , 0, 0 ;
+    cout << "Matrix sigma = " << endl << msigma << endl << "OKKKKKK" << endl;
+    
     // END YOUR CODE HERE
 
     // set t1, t2, R1, R2 
     // START YOUR CODE HERE
+    // Z轴顺时针90度旋转矩阵
+    Matrix3d Rz_c = Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
+    // Z轴逆时针90度旋转矩阵
+    Matrix3d Rz_a = Eigen::AngleAxisd(-M_PI / 2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
+    cout << "Rotation Matrix clockwise = " << endl << Rz_c <<endl;
+    cout << "Rotation Matrix anticlockwise = " << endl << Rz_a <<endl;
+
     Matrix3d t_wedge1;
     Matrix3d t_wedge2;
+    t_wedge1 = U * Rz_c * msigma * U.transpose();
+    t_wedge2 = U * Rz_a * msigma * U.transpose();
 
     Matrix3d R1;
     Matrix3d R2;
+
+    R1 = U * Rz_c.transpose() * V.transpose();
+    R2 = U * Rz_a.transpose() * V.transpose();
+
     // END YOUR CODE HERE
 
     cout << "R1 = " << R1 << endl;
