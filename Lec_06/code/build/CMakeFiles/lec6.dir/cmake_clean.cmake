790977190adeac9e4d2fad2c5ceb1703c69e5bd8
Elkulas <elkulasjiang@gmail.com>
2020-06-28 20:31:20 +0800

    update lec6 H1

---
+++ Lec_06/code/build/CMakeFiles/lec6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lec6.dir/main.cpp.o"
+  "lec6.pdb"
+  "lec6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lec6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lec_06/code/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jjj/Documents/SLAM_Basic_Lecture/Lec_06/code
+# Build directory: /home/jjj/Documents/SLAM_Basic_Lecture/Lec_06/code/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
